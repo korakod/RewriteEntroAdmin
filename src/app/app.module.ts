58ee38ba829a60c7341311923ca98f584c0191f7
TAN <tanyawit.p@entronica.co.th>
2017-06-06 15:22:22 +0700

    crate article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { EventsComponent } from './events/events.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    EventsComponent
+    EventsComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
