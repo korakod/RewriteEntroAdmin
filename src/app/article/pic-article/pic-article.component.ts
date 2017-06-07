d5341ba7451dd207d4611b6e0ad77f8d41a051ad
TAN <tanyawit.p@entronica.co.th>
2017-06-07 17:58:51 +0700

    Update code picArticle

---
+++ src/app/article/pic-article/pic-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pic-article',
+  templateUrl: './pic-article.component.html',
+  styleUrls: ['./pic-article.component.css']
+})
+export class PicArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
