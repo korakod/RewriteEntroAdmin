0d6063658a233d071b1f286f585def202c482638
GIIZBRELLIEN METEORA HYBRID <giizbrellien@gmail.com>
2017-06-07 16:21:08 +0700

    EVENTS--Add Weapon Feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { EventsComponent } from './events/events.component';
 import { ArticleComponent } from './article/article.component';
+import { NewWeaponComponent } from './events/new-weapon/new-weapon.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { ArticleComponent } from './article/article.component';
     ProductComponent,
     EventsComponent,
     ArticleComponent,
+    NewWeaponComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Weapon } from './weapon';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-events',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class EventsComponent implements OnInit {
- 
+  
+    weaponList = [
+      {linkImg:'https://vignette2.wikia.nocookie.net/warframe/images/d/da/TennoTonfa.png/revision/latest?cb=20140828170521',name:'Kronen',type:'melee',damage:'65'}
+      ,{linkImg:'https://vignette4.wikia.nocookie.net/warframe/images/e/e4/Miter.png/revision/latest/scale-to-width-down/220?cb=20130810114225',name:'Miter',type:'shotgun',damage:'125'}
+    ]
+
+
+  addNewWeapon(newWeapon){
+    if(newWeapon.length>0){
+    let weapon = {linkImg:'https://pbs.twimg.com/profile_images/584092186133667840/9i1QAPVJ.jpg',name: newWeapon,type: 'Undefind',damage: 'Undefind'};
+    this.weaponList.push(weapon);
+  }
+  }
+
   ngOnInit() {}
 
 }
